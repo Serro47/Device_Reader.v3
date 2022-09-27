3d5795f75d3f877c756441098054a1d4bc35653b
Serro47 <113613826+Serro47@users.noreply.github.com>
2022-09-27 15:52:18 +0200

    DB Connection is there

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import sqlite3
 import os
 import platform
-
-db = sqlite3.connect("device.db")
-cur = db.cursor()
-liste = cur.fetchall()
-
-cur.execute("""CREATE TABLE IF NOT EXISTS DEVICE (
-Hostname nvarchar
-);""")
+import mysql.connector
+from mysql.connector import Error
 
 
 class Environment:
@@ -21,18 +15,37 @@ class Environment:
         print(f"Devicetype: {self.DeviceName[11:13]}")
         print(f"Devicenumber: {self.DeviceName[13:15]}")
 
-
-
         if len(self.DeviceName) != 15:
             print("----------------------")
             print("Hostname not available!")
-            raise Exception("Error occurs")
 
 
         elif len(self.DeviceName) == 15:
             print("Hostname available!")
 
-        cur.execute("INSERT INTO DEVICE (Hostname) VALUES (?)", (Hostname,))
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='devicedb',
+                                         user='root',
+                                         password='')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+    print("MySQL connection is closed")
+    print("----------------------")
 
 Environment("Hostname")
-
